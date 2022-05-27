0f9f1faf8c3d0e9edd9914c19f3c3989ffbccb0d
Michal <mble@wp.pl>
2022-05-27 10:13:08 +0200

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -2,10 +2,13 @@ import typing_extensions
 
 import streamlit
 
-streamlit.title("Test")
+streamlit.title("My mom new health dinner")
 
 
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥑🐔 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🍞Hard-Boiled Free-Range Egg')
+
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
