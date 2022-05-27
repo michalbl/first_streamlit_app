28e0e344b5b0716e7b33d696f1163dddbad11f13
Michal <mble@wp.pl>
2022-05-27 10:11:22 +0200

    Update steamlit_app.py
    added breakfest menu 3 items

--- steamlit_app.py
+++ steamlit_app.py
@@ -3,3 +3,8 @@ import typing_extensions
 import streamlit
 
 streamlit.title("Test")
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
