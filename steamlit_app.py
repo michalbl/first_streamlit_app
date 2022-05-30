6624642dc76b48fb789c806b9267d53c858cac04
Michal <mble@wp.pl>
2022-05-30 15:53:49 +0200

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -29,10 +29,10 @@ streamlit.header("FruitVice Fruit Advice")
 fruit_choice  = streamlit.text_input('What fruit you would like te information about?', "Kiwi")
 streamlit.write("The user entered: ", fruit_choice)
 
-#fruitvice_reposnes = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+fruitvice_reposnes = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 
-#fruityvice_normalized = pandas.json_normalize(fruitvice_reposnes.json())
-#streamlit.dataframe(fruityvice_normalized)
+fruityvice_normalized = pandas.json_normalize(fruitvice_reposnes.json())
+streamlit.dataframe(fruityvice_normalized)
 
 
 
